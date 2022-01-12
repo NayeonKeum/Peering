5ee34806da28fc63f2ce2f8a15744c1423c12ecc
nkeum <rmaskdus0208@gmail.com>
2022-01-12 22:42:47 +0900

    initial commit

---
+++ app/src/main/java/com/awesomesol/peering/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.awesomesol.peering
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.awesomesol.peering.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // 전역 변수로 바인딩 객체 선언
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.tvMessage.setText("안녕")
+
+    }
+
+    // 액티비티가 파괴될 때..
+    override fun onDestroy() {
+        // onDestroy 에서 binding class 인스턴스 참조를 정리해주어야 한다.
+        mBinding = null
+        super.onDestroy()
+    }
+}
