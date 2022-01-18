6483162a55498509d90c9aab313a2f63cbd9283b
cyj2825 <cyj2825@naver.com>
2022-01-18 09:05:47 +0900

    FeedModel 생성

--- app/src/main/java/com/awesomesol/peering/feed/FeedFragment.kt
+++ app/src/main/java/com/awesomesol/peering/feed/FeedFragment.kt
@@ -5,29 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.awesomesol.peering.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FeedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +20,18 @@ class FeedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        val view = inflater.inflate(R.layout.fragment_feed, container, false)
+        val rv = view.findViewById<RecyclerView>(R.id.rv_FeedFragment)
+
+        val items = ArrayList<FeedModel>()
+        items.add(FeedModel("a", "b", "Kim", "안녕"))
+        items.add(FeedModel("a", "b", "Lee", "hihihi"))
+        items.add(FeedModel("a", "b", "Kang", "hello"))
+
+        rv.adapter = FeedRVAdapter(items)
+        rv.layoutManager = LinearLayoutManager(requireContext())
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FeedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FeedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/awesomesol/peering/feed/FeedRVAdapter.kt
+++ app/src/main/java/com/awesomesol/peering/feed/FeedRVAdapter.kt
@@ -3,10 +3,12 @@ package com.awesomesol.peering.feed
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.awesomesol.peering.R
 
-class FeedRVAdapter(val item : ArrayList<String>) : RecyclerView.Adapter<FeedRVAdapter.Viewholder>() {
+class FeedRVAdapter(val items : ArrayList<FeedModel>) : RecyclerView.Adapter<FeedRVAdapter.Viewholder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.feed_rv_item, parent, false)
@@ -14,16 +16,20 @@ class FeedRVAdapter(val item : ArrayList<String>) : RecyclerView.Adapter<FeedRVA
     }
 
     override fun onBindViewHolder(holder: FeedRVAdapter.Viewholder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindItems(items[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
     inner class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
         // feed_rv_item의 item의 값들을 하나하나 넣어주는 코드
-        fun bindItems(item : String){
+        fun bindItems(item : FeedModel){
+            val name = itemView.findViewById<TextView>(R.id.tv_FeedRVItem_nickname)
+            name.text = item.nickname
+            val content = itemView.findViewById<TextView>(R.id.tv_FeedRVItem_content)
+            content.text = item.content
 
         }
     }
