7a1ed3700fc15137f5fa0ffdd834d7bd3f6c0869
nkeum <rmaskdus0208@gmail.com>
2022-01-21 13:49:48 +0900

    [feat] 갤러리 관련 resolves:#26
    - [x] 갤러리 연동
    - [x] 사진 정보 받아와서 날짜 부분 파싱
    - [x] 필터링하여 날짜 지정해서 불러오기

--- app/src/main/java/com/awesomesol/peering/activity/SplashActivity.kt
+++ app/src/main/java/com/awesomesol/peering/activity/SplashActivity.kt
@@ -20,7 +20,7 @@ class SplashActivity : AppCompatActivity() {
 
     }
     companion object {
-        private const val DURATION : Long = 3000
+        private const val DURATION : Long = 1500
     }
 
     override fun onBackPressed() {
