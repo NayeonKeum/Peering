5931d0dcdbe7035c137cb1ec9aecb1ca0e5866ca
nkeum <rmaskdus0208@gmail.com>
2022-01-29 20:05:51 +0900

    viewpager로 이미지 슬라이더 넣음!

--- app/src/main/java/com/awesomesol/peering/calendar/PostFragment.kt
+++ app/src/main/java/com/awesomesol/peering/calendar/PostFragment.kt
@@ -9,22 +9,24 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.text.format.DateFormat
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.awesomesol.peering.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
+
 
 class PostFragment : Fragment() {
 
@@ -39,6 +41,18 @@ class PostFragment : Fragment() {
     private lateinit var bottomSheetBehavior:BottomSheetBehavior<View>
 
 
+    // 슬라이더
+    private var sliderViewPager: ViewPager2? = null
+    private var layoutIndicator: LinearLayout? = null
+
+    private val images = arrayOf(
+        "https://cdn.pixabay.com/photo/2019/12/26/10/44/horse-4720178_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/11/04/15/29/coffee-beans-5712780_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/03/08/21/41/landscape-4913841_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/09/02/18/03/girl-5539094_1280.jpg",
+        "https://cdn.pixabay.com/photo/2014/03/03/16/15/mosque-279015_1280.jpg"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //권한 체크
@@ -46,13 +60,15 @@ class PostFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view=inflater.inflate(R.layout.fragment_post, container, false)
 
-        val rv:RecyclerView=view.findViewById<View>(R.id.bottomsheetview).findViewById<RecyclerView>(R.id.rv_PostFragment)
+        val rv:RecyclerView=view.findViewById<View>(R.id.bottomsheetview).findViewById<RecyclerView>(
+            R.id.rv_PostFragment
+        )
         Log.d(TAG, rv.toString())
         //rv.layoutManager=GridLayoutManager(context, 3)
         rv.layoutManager=LinearLayoutManager(context).also{it.orientation=LinearLayoutManager.HORIZONTAL}
@@ -64,7 +80,7 @@ class PostFragment : Fragment() {
         rv.adapter=galleryRVAdapter
 
 
-        Log.d(TAG, "targetDate에 있는 사진 개수: "+calendarImages.get(targetDate)?.size)
+        Log.d(TAG, "targetDate에 있는 사진 개수: " + calendarImages.get(targetDate)?.size)
         val datasize: Int? =calendarImages.get(targetDate)?.size
 
         for (i : Int in 0..(datasize!!-1)){
@@ -85,10 +101,29 @@ class PostFragment : Fragment() {
         // peekHight 만큼
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
-        inflater.inflate(R.layout.fragment_post_bottomsheet_photos, container).findViewById<ImageView>(R.id.iv_bottomsheet_up).setOnClickListener {
+        inflater.inflate(R.layout.fragment_post_bottomsheet_photos, container).findViewById<ImageView>(
+            R.id.iv_bottomsheet_up
+        ).setOnClickListener {
             // 전체 보여주기
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
+
+
+        sliderViewPager = view.findViewById(R.id.sliderViewPager)
+        layoutIndicator = view.findViewById(R.id.layoutIndicators)
+
+        sliderViewPager!!.offscreenPageLimit = 1
+        sliderViewPager!!.adapter = ImageSliderAdapter(requireContext(), images)
+
+        sliderViewPager!!.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+
+        setupIndicators(images.size)
+
 
 
         return view
@@ -100,6 +135,7 @@ class PostFragment : Fragment() {
 
     }
 
+    // 바텀시트에 리사이클러뷰 어댑터
     class GalleryRVAdapter(var context: Context):RecyclerView.Adapter<GalleryRVAdapter.ViewHolder>() {
 
         var dataList = emptyList<GalleryData>()
@@ -124,7 +160,7 @@ class PostFragment : Fragment() {
             }
         }
 
-        fun setView(parentView:View){
+        fun setView(parentView: View){
             this.parentView=parentView
         }
 
@@ -132,8 +168,16 @@ class PostFragment : Fragment() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryRVAdapter.ViewHolder {
 
             // Inflate the custom layout
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_post_rv_item, parent, false)
-            iv_PostFragment=LayoutInflater.from(context).inflate(R.layout.fragment_post, null, false).findViewById(R.id.iv_PostFragment)
+            var view = LayoutInflater.from(parent.context).inflate(
+                R.layout.fragment_post_rv_item,
+                parent,
+                false
+            )
+            iv_PostFragment=LayoutInflater.from(context).inflate(
+                R.layout.fragment_post,
+                null,
+                false
+            ).findViewById(R.id.iv_PostFragment)
             return ViewHolder(view)
         }
 
@@ -149,32 +193,64 @@ class PostFragment : Fragment() {
             holder.iv.setOnClickListener {
                 var iv_PostFragment=parentView.findViewById<ImageView>(R.id.iv_PostFragment)
                 iv_PostFragment.setImageURI(data.imageUri)
-                Log.d(TAG, "이미지뷰 찾음? $iv_PostFragment")
-
-//                val frg= fragmentManager.findFragmentById(R.id.fragment_post);
-//                val ft = fragmentManager.beginTransaction();
-//                // Log.d(TAG, frg.toString())
-//                if (frg != null) {
-//                    Log.d(TAG, "프랙 찾음")
-//                    ft.detach(frg)
-//                    ft.attach(frg);
-//                    ft.commit();
-//                };
-
-
-//            fun ViewGroup.inflate(resId: Int, attach: Boolean = false): View
-//                    = LayoutInflater.from(context).inflate(R.layout.fragment_post, this, false)
             }
-
         }
-
         override fun getItemId(position: Int): Long {
             return super.getItemId(position)
         }
-
         //  total count of items in the list
         override fun getItemCount() = dataList.size
     }
+
+    // 이미지 넘어가는 뷰페이지 어댑터
+    private fun setupIndicators(count: Int) {
+        val indicators = arrayOfNulls<ImageView>(count)
+        val params = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        params.setMargins(16, 8, 16, 8)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(context)
+            indicators[i]!!.setImageDrawable(
+                context?.let {
+                    ContextCompat.getDrawable(
+                        it,
+                        R.drawable.bg_indicator_inactive
+                    )
+                }
+            )
+            indicators[i]!!.layoutParams = params
+            layoutIndicator!!.addView(indicators[i])
+        }
+        setCurrentIndicator(0)
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = layoutIndicator!!.childCount
+        for (i in 0 until childCount) {
+            val imageView = layoutIndicator!!.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    context?.let {
+                        ContextCompat.getDrawable(
+                            it,
+                            R.drawable.bg_indicator_active
+                        )
+                    }
+                )
+            } else {
+                imageView.setImageDrawable(
+                    context?.let {
+                        ContextCompat.getDrawable(
+                            it,
+                            R.drawable.bg_indicator_inactive
+                        )
+                    }
+                )
+            }
+        }
+    }
+
 
     private fun checkPermission() {
         if (activity?.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -185,9 +261,9 @@ class PostFragment : Fragment() {
         }
     }
     override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
     ) {
         when (requestCode) {
             200 -> {
@@ -221,9 +297,9 @@ class PostFragment : Fragment() {
 
         //가져올 컬럼명
         val what = arrayOf(
-                MediaStore.Images.ImageColumns._ID,
-                MediaStore.Images.ImageColumns.TITLE,
-                MediaStore.Images.ImageColumns.DATE_TAKEN
+            MediaStore.Images.ImageColumns._ID,
+            MediaStore.Images.ImageColumns.TITLE,
+            MediaStore.Images.ImageColumns.DATE_TAKEN
         )
 
         //정렬
@@ -254,7 +330,10 @@ class PostFragment : Fragment() {
 //                val imageUri =
 //                        withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-                var uri= ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                var uri= ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
 
                 //3. 데이터를 View로 설정
                 val calendar = Calendar.getInstance()
