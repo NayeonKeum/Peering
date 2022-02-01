4b92ec2981f25127deac5233caab18b095bd55be
nkeum <rmaskdus0208@gmail.com>
2022-02-01 20:22:52 +0900

    이미지 중복초기화 수정, 배경에 croptoline true로 설정함

--- app/src/main/java/com/awesomesol/peering/calendar/PostFragment.kt
+++ app/src/main/java/com/awesomesol/peering/calendar/PostFragment.kt
@@ -16,6 +16,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
@@ -45,6 +46,7 @@ class PostFragment : Fragment() {
     // 슬라이더
     private var sliderViewPager: ViewPager2? = null
     private var layoutIndicator: LinearLayout? = null
+    private lateinit var cl_PostFragment: ConstraintLayout
 
     private var images:ArrayList<String> = arrayListOf()
 
@@ -106,6 +108,9 @@ class PostFragment : Fragment() {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
 
+        // 둥근 모서리
+        cl_PostFragment=view.findViewById(R.id.cl_PostFragment)
+        cl_PostFragment.clipToOutline=true
 
         // 초기에 해주는 거!
         sliderViewPager = view.findViewById(R.id.sliderViewPager)
@@ -144,6 +149,7 @@ class PostFragment : Fragment() {
         lateinit var parentView: View
 
         lateinit var sliderViewPager:ViewPager2
+        lateinit var cl_PostFragment:ConstraintLayout
         lateinit var layoutIndicator:LinearLayout
 
         internal fun setDataList(dataList: List<GalleryData>) {
@@ -164,6 +170,7 @@ class PostFragment : Fragment() {
             this.parentView=parentView
             sliderViewPager = parentView.findViewById(R.id.sliderViewPager)
             layoutIndicator = parentView.findViewById(R.id.layoutIndicators)
+            cl_PostFragment = parentView.findViewById(R.id.cl_PostFragment)
         }
 
         // Usually involves inflating a layout from XML and returning the holder
@@ -216,8 +223,11 @@ class PostFragment : Fragment() {
                         useImages.add(dataList[i].imageUri.toString())
                     }
                 }
+                layoutIndicator!!.removeAllViews()
 
-                sliderViewPager = parentView.findViewById(R.id.sliderViewPager)
+                // 둥근 모서리
+                cl_PostFragment.clipToOutline=true
+
                 sliderViewPager!!.adapter = ImageSliderAdapter(context, useImages)
                 sliderViewPager!!.registerOnPageChangeCallback(object : OnPageChangeCallback() {
                     override fun onPageSelected(position: Int) {
