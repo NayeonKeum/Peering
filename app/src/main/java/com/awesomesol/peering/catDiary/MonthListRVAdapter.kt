bfe52026301b9dd278c192c1eb51daa2102a8c3c
seohyun319 <tjguselly0319@gmail.com>
2022-02-02 16:32:21 +0900

    FEAT: add month_list_rv_item RecyclerView

---
+++ app/src/main/java/com/awesomesol/peering/catDiary/MonthListRVAdapter.kt
@@ -0,0 +1,33 @@
+package com.awesomesol.peering.catDiary
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.awesomesol.peering.R
+
+class MonthListRVAdapter(val items: ArrayList<String>) : RecyclerView.Adapter<MonthListRVAdapter.Viewholder>() {
+    // 아이템 하나(month_list_rv_item.xml) 갖고와서 하나의 레이아웃 만들어줌
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MonthListRVAdapter.Viewholder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.month_list_rv_item, parent, false)
+        return Viewholder(v)
+    }
+
+    override fun onBindViewHolder(holder: MonthListRVAdapter.Viewholder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // month_list_rv_item.xml에 하나하나씩 리턴 넣어주는 역할. 아이템의 내용물 넣어주기
+    inner class Viewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(item: String) {
+
+        }
+    }
+}
--- app/src/main/java/com/awesomesol/peering/catDiary/catDiaryFragment.kt
+++ app/src/main/java/com/awesomesol/peering/catDiary/catDiaryFragment.kt
@@ -1,25 +1,22 @@
 package com.awesomesol.peering.catDiary
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.awesomesol.peering.R
-import com.kakao.sdk.talk.TalkApiClient
 
 
 class catDiaryFragment : Fragment() {
-    // 우리 친구... 이메일로 초대 보내느건가?!?! 어떻게 보내는 거였지...
 
     val TAG="캣다"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        
 
     }
 
@@ -28,7 +25,25 @@ class catDiaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catdiary, container, false)
+        val view = inflater.inflate(R.layout.fragment_catdiary, container, false)
+
+        // RecyclerView 생성
+        val rv = view.findViewById<RecyclerView>(R.id.rv_catDiaryFragment_monthList)
+
+        // ContentRVAdapter의 인자인 items 만들어주기
+        val items = ArrayList<String>()
+        items.add("a")
+        items.add("b")
+        items.add("c")
+
+        // rv의 adapter는 여기에서 만든 Adapter이다~
+        rv.adapter = MonthListRVAdapter(items)
+
+        rv.layoutManager = LinearLayoutManager(requireContext())
+//        rv.layoutManager = GridLayoutManager(this, 6)
+
+        return view
     }
+
 
 }
