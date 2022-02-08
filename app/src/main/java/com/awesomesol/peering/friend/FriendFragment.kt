a21a74deafe8c02d5e56f6f1fc33e8ae0683b197
yena777 <parkyena01@sookmyung.ac.kr>
2022-02-08 22:38:03 +0900

    friends 기본 정적 ui 구성

--- app/src/main/java/com/awesomesol/peering/friend/FriendFragment.kt
+++ app/src/main/java/com/awesomesol/peering/friend/FriendFragment.kt
@@ -1,16 +1,20 @@
 package com.awesomesol.peering.friend
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.awesomesol.peering.R
 import kotlinx.android.synthetic.main.fragment_feed.*
+import kotlinx.android.synthetic.main.fragment_friend.*
 
-class FeedFragment : Fragment() {
+class FriendFragment : Fragment() {
     val TAG="피드"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,19 +39,18 @@ class FeedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_feed, container, false)
-        val rv = view.findViewById<RecyclerView>(R.id.rv_FeedFragment)
+        val view = inflater.inflate(R.layout.fragment_friend, container, false)
+        val rv = view.findViewById<RecyclerView>(R.id.rv_FriendFragment)
 
-        val items = ArrayList<FeedModel>()
-        items.add(FeedModel("a", "b", "Kim", "오늘은 따사로운 햇살과 함께 코딩을 했다. 낭만 속의 비낭만..... 소리 없는 아우성..... 집인데 집에 가고 싶었다...하하핳"))
-        items.add(FeedModel("a", "b", "Lee", "hihihi"))
-        items.add(FeedModel("a", "b", "Kang", "hello"))
-        items.add(FeedModel("a", "b", "Cho", "hey"))
-        items.add(FeedModel("a", "b", "Park", "yoyo"))
+        val items = ArrayList<FriendModel>()
+        items.add(FriendModel("a", "b",  "c", "d","Kim", "abc@gmail.com"))
+        items.add(FriendModel("a", "b", "c", "d","Lee", "abc@gmail.com"))
+        items.add(FriendModel("a", "b", "c", "d","Kang", "abc@gmail.com"))
+        items.add(FriendModel("a", "b", "c", "d","Cho", "abc@gmail.com"))
+        items.add(FriendModel("a", "b", "c", "d","Park", "abc@gmail.com"))
 
-        rv.adapter = FeedRVAdapter(items)
+        rv.adapter = FriendRVAdapter(items)
         rv.layoutManager = LinearLayoutManager(requireContext())
 
         return view
@@ -60,11 +63,11 @@ class FeedFragment : Fragment() {
     }
     // recyclerview 클릭 수정
     fun moreBtnClick(){
-        iv_FeedFragment_friends.setOnClickListener {
-            //val diaryreadFragment = DiaryReadFragment()
-            val friendFragment = FriendFragment()
+        iv_FriendFragment_friends.setOnClickListener {
+            val diaryreadFragment = DiaryReadFragment()
+
             requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.main_screen_panel, friendFragment).commitNow()
+                .replace(R.id.main_screen_panel, diaryreadFragment).commitNow()
         }
     }
 
